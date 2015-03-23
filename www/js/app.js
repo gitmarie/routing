1a9dbb4882ae5b276ccd35c412408e0c34d07c50
mariekeely <marie_keely@yahoo.co.uk>
2015-03-23 14:36:28 +0000

    added states

--- www/js/app.js
+++ www/js/app.js
@@ -3,6 +3,39 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
+
+var app = angular.module('ionicApp', ['ionic'])
+
+// both the $stateProvider and $urlRouterProvider provide the basic blocks necessary to configure our application’s state machine
+app.config(function ($stateProvider, $urlRouterProvider) {
+
+    // declaring state
+    $urlRouterProvider.otherwise('/home')
+
+    // when this is run, the template is injected where ion-nav-view exists in html
+    
+    // state transition 1
+    $stateProvider.state('home', {
+        url: '/home',
+        views: {
+            help: {
+                templateUrl: 'home.html'
+            }
+        }
+    })
+
+    // state transition 2
+    $stateProvider.state('help', {
+        url: '/help',
+        views: {
+            help: {
+                templateUrl: 'help.html'
+            }
+        }
+    });
+})
+
+/*
 angular.module('starter', ['ionic'])
 
 .run(function($ionicPlatform) {
@@ -17,3 +50,4 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+*/
